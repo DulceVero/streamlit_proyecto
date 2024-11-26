6f4b59b096cd3eeff3efd77153c0ca49a9f4588d
Cindybell <110927209+DulceVero@users.noreply.github.com>
2024-11-26 15:24:15 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
 
 st.write("Esto es una linea de Prueba")
 st.text_input('dato')
 
-st.write("Below is a DataFrame:", data_frame, "Above is a dataframe.")
+
+st.write(1234)
+st.write(
+    pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40],
+        }
+    )
+)
