e9e39a8a0ca56064230a0b0d24117eda1127ec56
Cindybell <110927209+DulceVero@users.noreply.github.com>
2024-11-28 14:38:38 -0400

    Update file.py

--- file.py
+++ file.py
@@ -3,5 +3,9 @@ import streamlit as st
 st.write("Esto es una linea de Prueba")
 st.text_input('dato')
 
-
+option = st.selectbox(
+  "Tipo de vehículo que usa",
+  ("Moto", "Carro", "Camión", "Helicóptero"),
+)
+st.write("Su elección fue:", option)
 
