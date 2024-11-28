e9a6e79924dbb1d0515635122ee4d1c3e2c2db9d
Cindybell <110927209+DulceVero@users.noreply.github.com>
2024-11-28 12:03:22 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,17 +1,7 @@
 import streamlit as st
-import pandas as pd
 
 st.write("Esto es una linea de Prueba")
 st.text_input('dato')
 st.selectbox()
 
 
-st.write(1234)
-st.write(
-    pd.DataFrame(
-        {
-            "first column": [1, 2, 3, 4],
-            "second column": [10, 20, 30, 40],
-        }
-    )
-)
