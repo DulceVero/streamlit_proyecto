2004ef5987cca8a9aaf61a7f1e08cfb7a9f14586
Cindybell <110927209+DulceVero@users.noreply.github.com>
2024-11-29 11:13:30 -0400

    Update file.py

--- file.py
+++ file.py
@@ -14,3 +14,11 @@ tipo = st.selectbox(
   "Tipo de animal",
   ("Hervivoro", "Carnivoro"),
 )
+
+st.title("My Awesome App")
+
+@st.fragment()
+def toggle_and_text():
+    cols = st.columns(2)
+    cols[0].toggle("Toggle")
+    cols[1].text_area("Enter text")
