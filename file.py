78226efd7ecd6584f55c21cf27cfa86d9952c6a8
Cindybell <110927209+DulceVero@users.noreply.github.com>
2024-12-02 10:26:17 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,26 +1,5 @@
 import streamlit as st
 
-formulario = st.form('practica')
-
-st.write("Esto es una linea de Prueba")
-
-
-def datos():
-  cols = st.colums(2)
-  cols[0].text_input('dato')
-  cols[1].text_input('dato2')
-
-
-option = st.selectbox(
-  "Tipo de vehículo que usa",
-  ("Moto", "Carro", "Camión", "Helicóptero"),
-)
-
-tipo = st.selectbox(
-  "Tipo de animal",
-  ("Hervivoro", "Carnivoro"),
-)
-
 st.title("Formulario Básico en Streamlit")
 
 with st.form("my_form"):
