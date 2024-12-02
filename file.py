a07b0c1f4cc8b49720571b44bfa1ea1aaf2f957e
Cindybell <110927209+DulceVero@users.noreply.github.com>
2024-12-02 11:42:23 -0400

    Update file.py

--- file.py
+++ file.py
@@ -4,17 +4,22 @@ st.title("Activación del Comité de Vivienda")
 
 st.title("Datos de Voceros Activos")
 with st.form("my_form"):
-    col1, col2 = st.columns(2)
+    col1, col2, col3, col4 = st.columns(2)
 
     with col1:
         nombre = st.text_input("Nombre")
         apellido = st.text_input("Apellido")
+
+    with col2:
         cedula = st.text_input("Cédula")
         correo = st.email_input("Correo")
 
-    with col2:
-        edad = st.number_input("Edad")
-        pais = st.selectbox("País", ["España", "México", "Argentina", "Estados Unidos"])
+    with col3:
+        telefono = st.text_input("Número de Telefóno")
+        edad = st.text_input("Edad")
 
+    with col4:
+        sexo = st.selectbox("Sexo", ["MASCULUINO", "FEMENINO"])
+        tipo_voceria = st.radio("Tipo de Vocería", ["Principal", "Suplente"])
     # Botón para enviar el formulario
     submitted = st.form_submit_button("Guardar")
