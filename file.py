733b68d5fb1bcefaebbdac286aacf59998319eed
Cindybell <110927209+DulceVero@users.noreply.github.com>
2024-12-02 09:31:04 -0400

    Update file.py

--- file.py
+++ file.py
@@ -20,3 +20,19 @@ tipo = st.selectbox(
   "Tipo de animal",
   ("Hervivoro", "Carnivoro"),
 )
+
+st.title("Formulario Básico en Streamlit")
+
+with st.form("my_form"):
+    col1, col2 = st.columns(2)
+
+    with col1:
+        nombre = st.text_input("Nombre")
+        email = st.text_input("Email")
+
+    with col2:
+        edad = st.number_input("Edad")
+        pais = st.selectbox("País", ["España", "México", "Argentina", "Estados Unidos"])
+
+    # Botón para enviar el formulario
+    submitted = st.form_submit_button("Enviar")
